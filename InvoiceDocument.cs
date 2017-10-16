dd23f47cb30d308fe907d73ef6a2f61bb5c26cf0
Willy <Willy@DESKTOP-KMHF2U1>
2017-10-17 03:28:07 +0800

    part 4

--- InvoiceDocument.cs
+++ InvoiceDocument.cs
@@ -164,7 +164,7 @@ namespace prototype2
             customerName.Format.Font.Name = "Calibri";
             customerName.Format.Font.Size = 11;
             customerName.Format.Font.Bold = true;
-            addressFrame.AddFormattedText(MainVM.SelectedCustomerSupplier.CompanyAddress + "\t" + MainVM.SelectedCustomerSupplier.CompanyCity + "\t" + MainVM.SelectedCustomerSupplier.CompanyProvinceName);
+            addressFrame.AddFormattedText(MainVM.SelectedCustomerSupplier.CompanyAddress +"," + "\t" + MainVM.SelectedCustomerSupplier.CompanyCity + "," + "\t" + MainVM.SelectedCustomerSupplier.CompanyProvinceName);
             addressFrame.Format.Font.Name = "Calibri";
             addressFrame.Format.Font.Size = 11;
 
