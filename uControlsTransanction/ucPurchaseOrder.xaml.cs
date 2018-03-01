45fe72b0a78120495c177294dde215146be8b0c0
Lewis Ken K <kenlewis0254@gmail.com>
2018-03-02 05:49:45 +0800

    03/02/2018 - Purchase Order Ongoing

--- uControlsTransanction/ucPurchaseOrder.xaml.cs
+++ uControlsTransanction/ucPurchaseOrder.xaml.cs
@@ -45,17 +45,6 @@ namespace prototype2
                 handler(this, e);
         }
 
-        private void selectSupplierBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OnSelectCustomerClicked(e);
-        }
-
-
-        private void selectItemsBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OnSelectSalesQuote(e);
-        }
-
         private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (this.IsLoaded && this.IsVisible)
@@ -70,8 +59,95 @@ namespace prototype2
                         obj.Visibility = Visibility.Visible;
                 }
             }
-            
+
         }
 
+        private void selectSupplierBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OnSelectCustomerClicked(e);
+        }
+
+
+        private void selectItemsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OnSelectSalesQuote(e);
+        }
+
+        
+
+        private void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (UIElement obj in containerGrid.Children)
+            {
+                if (containerGrid.Children.IndexOf(obj) == 0)
+                {
+                    nextBtn.Content = "Save";
+                }
+                else if (containerGrid.Children.IndexOf(obj) == 1)
+                {
+                    saveDataToDb();
+                }
+            }
+        }
+
+        private void saveDataToDb()
+        {
+            var dbCon = DBConnection.Instance();
+            bool noError = true;
+            string poNumChar = "TRANS" + DateTime.Now.ToString("yyyy-MM-dd");
+            if (dbCon.IsConnect())
+            {
+                string query = "INSERT purchase_order_t (`PONumChar`,`suppID`,`shipTo`, `POdueDate`,`shipVia`, `requisitioner`, `incoterms`, `POstatus`, `currency`, `importantNotes`, `preparedBy`, `approveBy`, `refNo`, `termsDays`, `termsDP`) VALUES " +
+                    "('" + poNumChar +"'," +
+                    MainVM.SelectedCustomerSupplier.CompanyID + "," +
+                    "'','" +
+                    selectedDateRequiredTb.SelectedDate.Value.ToString("yyyy-MM-dd") +"','" +
+                    shipViaCb.SelectedValue.ToString() + "','" +
+                    "'," +
+                    "," +
+                    "," +
+                    "," +
+                    "," +
+                    "," +
+                    "," +
+                    "," +
+                    "," +
+                    "" +
+                    ");";
+                if (dbCon.insertQuery(query, dbCon.Connection))
+                {
+                    if (dbCon.IsConnect())
+                    {
+                        MessageBox.Show("Successfully added.");
+                    }
+
+                }
+            }
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(UIElement obj in containerGrid.Children)
+            {
+                if (containerGrid.Children.IndexOf(obj) == 0)
+                {
+                    
+                }
+                else if(containerGrid.Children.IndexOf(obj) == 1)
+                {
+
+                }
+            }
+        }
+
+        private void paymentCustomRb_Checked(object sender, RoutedEventArgs e)
+        {
+            downpaymentPercentTb.IsEnabled = true;
+        }
+
+        private void paymentCustomRb_Unchecked(object sender, RoutedEventArgs e)
+        {
+            downpaymentPercentTb.IsEnabled = false;
+        }
     }
 }
